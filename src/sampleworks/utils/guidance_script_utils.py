71e6f0fe62f7b08c8fdc0156932b9672c8a97b83
Marcus D. Collins <marcus.collins@astera.org>
2025-12-16 18:40:52 +0000

    Addressing PR 25 comments

--- src/sampleworks/utils/guidance_script_utils.py
+++ src/sampleworks/utils/guidance_script_utils.py
@@ -3,25 +3,24 @@ from __future__ import annotations
 import argparse
 import os
 import pickle
-import time
 import traceback
 from datetime import datetime
-from typing import Any
-from atomworks import parse
-from loguru import logger
 from pathlib import Path
+from typing import Any
 
 import torch
-
-from biotite.structure import AtomArray, stack
+from atomworks import parse
+from biotite.structure import AtomArray, AtomArrayStack, stack
 from biotite.structure.io import save_structure
+from loguru import logger
 
 from sampleworks.core.forward_models.xray.real_space_density_deps.qfit.volume import XMap
 from sampleworks.core.rewards.real_space_density import RewardFunction, setup_scattering_params
 from sampleworks.core.scalers.fk_steering import FKSteering
 from sampleworks.core.scalers.pure_guidance import PureGuidance
-from sampleworks.utils.checkpoint_utils import get_checkpoint
+from sampleworks.utils.guidance_constants import BOLTZ_1, BOLTZ_2
 from sampleworks.utils.guidance_script_arguments import GuidanceConfig, JobResult
+
 
 # The following imports aren't compatible with each other and are supported in separate
 # hatch/pixi envs
@@ -44,32 +43,33 @@ def save_trajectory(
     output_dir,
     reward_param_mask,
     subdir_name,
-    save_every=10
+    save_every=10,
 ):
     if scaler_type == "pure_guidance":
         _save_trajectory(
-            trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every=save_every
+            trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every
         )
     elif scaler_type == "fk_steering":
         _save_fk_steering_trajectory(
-            trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every=save_every
+            trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every
         )
     else:  # we shouldn't ever get here, since we can't have run guidance w/o this!
         raise ValueError(f"Invalid scaler type: {scaler_type}")
 
 
 def _save_trajectory(
-    trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every=10
+    trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every
 ):
-
     output_dir = Path(output_dir / "trajectory" / subdir_name)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # TODO: @marcus.collins swap try/except for if/else in next PR
-    try:
-        assert isinstance(atom_array, AtomArray)
-    except AssertionError:
+    if isinstance(atom_array, AtomArrayStack):
         atom_array = atom_array[0]
+    if not isinstance(atom_array, AtomArray):
+        raise TypeError(
+            "Can only save a trajectory of type AtomArray or "
+            f"AtomArrayStack, was given {type(atom_array)}"
+        )
 
     for i, coords in enumerate(trajectory):
         ensemble_size = coords.shape[0]
@@ -82,15 +82,18 @@ def _save_trajectory(
 
 
 def _save_fk_steering_trajectory(
-    trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every=10
+    trajectory, atom_array, output_dir, reward_param_mask, subdir_name, save_every
 ):
     output_dir = Path(output_dir / "trajectory" / subdir_name)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    try:
-        assert isinstance(atom_array, AtomArray)
-    except AssertionError:
+    if isinstance(atom_array, AtomArrayStack):
         atom_array = atom_array[0]
+    if not isinstance(atom_array, AtomArray):
+        raise TypeError(
+            "Can only save a trajectory of type AtomArray or "
+            f"AtomArrayStack, was given {type(atom_array)}"
+        )
 
     for i, coords in enumerate(trajectory):
         ensemble_size = coords.shape[1]  # first dim is the particle dim
@@ -118,59 +121,58 @@ def save_losses(losses, output_dir):
 
 
 def get_model_and_device(
-        device: str,
-        model_checkpoint_path: str,
-        model_type: str,
-        method: str | None = None,
-        model: Any = None,
+    device_str: str,
+    model_checkpoint_path: str,
+    model_type: str,
+    method: str | None = None,
+    model: Any = None,
 ) -> tuple[torch.device, Any]:
-    device = torch.device(device) if device else try_gpu()
+    device = torch.device(device_str) if device_str else try_gpu()
     logger.debug(f"Using device: {device}")
     if model_type == "protenix":
         logger.debug(f"Loading Protenix model from {model_checkpoint_path}")
-        model_wrapper = ProtenixWrapper(
-            checkpoint_path=model_checkpoint_path,
-            device=device,
-            model=model
+        model_wrapper = ProtenixWrapper(  # pyright: ignore
+            checkpoint_path=model_checkpoint_path, device=device, model=model
         )
     elif model_type == "boltz1":
         logger.debug(f"Loading Boltz1 model from {model_checkpoint_path}")
-        model_wrapper = Boltz1Wrapper(
+        model_wrapper = Boltz1Wrapper(  # pyright: ignore
             checkpoint_path=model_checkpoint_path,
             use_msa_server=True,
             device=device,
-            model=model
+            model=model,
         )
     elif model_type == "boltz2":
         if method is None:
             # TODO: make a useful error msg that includes options for method
             raise ValueError("Method must be specified for Boltz2")
         logger.debug(f"Loading Boltz2 model from {model_checkpoint_path}")
-        model_wrapper = Boltz2Wrapper(
+        model_wrapper = Boltz2Wrapper(  #  pyright: ignore
             checkpoint_path=model_checkpoint_path,
             use_msa_server=True,
             device=device,
             method=method.upper(),
-            model=model
+            model=model,
         )
     else:
         raise ValueError(f"Unknown model type: {model_type}")
 
-    return device, model_wrapper
+    # (pyright doesn't think Boltz1Wrapper etc are "Any")
+    return (device, model_wrapper)  # pyright: ignore
 
 
 # TODO: further atomize for easier testing.
 def get_reward_function_and_structure(
-        density: str | Path,
-        device: torch.device,
-        em,
-        loss_order,
-        resolution,
-        structure: str | Path
+    density: str | Path,
+    device: torch.device,
+    em,
+    loss_order,
+    resolution,
+    structure_path: str | Path,
 ) -> tuple[RewardFunction, dict[str, Any]]:
-    logger.debug(f"Loading structure from {structure}")
+    logger.debug(f"Loading structure from {structure_path}")
     structure = parse(
-        structure,
+        structure_path,  # pyright: ignore  (doesn't like the type being passed)
         hydrogen_policy="remove",
         add_missing_atoms=False,
         ccd_mirror_path=None,
@@ -180,9 +182,9 @@ def get_reward_function_and_structure(
     xmap = XMap.fromfile(density, resolution=resolution)
 
     logger.debug("Setting up scattering parameters")
-    scattering_params = setup_scattering_params(structure, em=em)
+    scattering_params = setup_scattering_params(structure, em=em)  # pyright: ignore
 
-    atom_array = structure["asym_unit"]
+    atom_array = structure["asym_unit"]  # pyright: ignore
     selection_mask = atom_array.occupancy > 0
     n_selected = selection_mask.sum()
     logger.info(f"Selected {n_selected} atoms with occupancy > 0")
@@ -200,13 +202,13 @@ def get_reward_function_and_structure(
 
 
 def save_everything(
-        output_dir: str | Path,
-        losses: list[Any],
-        refined_structure: dict,
-        traj_denoised: list[Any],
-        traj_next_step: list[Any],
-        scaler_type: str
-        ) -> None:
+    output_dir: str | Path,
+    losses: list[Any],
+    refined_structure: dict,
+    traj_denoised: list[Any],
+    traj_next_step: list[Any],
+    scaler_type: str,
+) -> None:
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -252,45 +254,9 @@ def save_everything(
 #####################
 # TODO: these args are ultimately defined in run_grid_search.py, which is a terrible place
 #  for that. Need to refactor this.
-# TODO: can't seem to get the delay to work properly...
-def setup_guidance_worker(args, model_name, delay=0.0):
-    global device, model_wrapper  # pylint: disable=global-statement
-
-    # Might want to introduce a random delay here to avoid all workers starting at the same time.
-    # this would allow the method below to find a free GPU and avoid a race?
-    logger.info(f"Waiting {delay} seconds before initializing model")
-    time.sleep(delay)
-
-    if hasattr(args, "model_checkpoint"):
-        model_checkpoint_path = args.model_checkpoint
-    else:
-        model_checkpoint_path = get_checkpoint(model_name, args)
-
-    if hasattr(args, "method"):
-        method = args.method
-    elif hasattr(args, "methods") and len(methods := args.methods.split(",")) == 1:
-        method = methods[0]
-    else:
-        raise ValueError(
-            "setup_guidance_worker requires either --method or --methods to be a "
-            "single string with no commas"
-        )
-
-    device, model_wrapper = get_model_and_device(
-        device="",  # automatically look for a free GPU
-        model_checkpoint_path=model_checkpoint_path,
-        model_type=model_name,
-        method=method if model_name == "boltz2" else None
-    )
-
-    logger.info(
-        f"Model {model_name} initialized with hash {model_wrapper.__hash__()} on device: {device}"
-    )
-    return None
-
-
-def run_guidance(args: GuidanceConfig | argparse.Namespace, guidance_type: str,
-                 model_wrapper, device) -> JobResult:
+def run_guidance(
+    args: GuidanceConfig | argparse.Namespace, guidance_type: str, model_wrapper, device
+) -> JobResult:
     """
     Wrapper around the actual _run_guidance function, to redirect logs and generate a JobResult.
     Args:
@@ -299,7 +265,7 @@ def run_guidance(args: GuidanceConfig | argparse.Namespace, guidance_type: str,
 
     Returns:
     """
-    #global device
+
     os.makedirs(os.path.dirname(args.log_path), exist_ok=True)
 
     # need to finish spawning separate logs for each worker
@@ -319,19 +285,22 @@ def run_guidance(args: GuidanceConfig | argparse.Namespace, guidance_type: str,
     finally:
         logger.remove(handle)
 
-# "guidance_type" is also called "scaler" in many places
-def _run_guidance(args: GuidanceConfig | argparse.Namespace, guidance_type: str,
-                  model_wrapper, device):
-    #global device, model_wrapper
 
+# "guidance_type" is also called "scaler" in many places
+def _run_guidance(
+    args: GuidanceConfig | argparse.Namespace, guidance_type: str, model_wrapper, device
+):
     reward_function, structure = get_reward_function_and_structure(
         args.density,  # str/path to a map file.
         device,  # this needs to come from the global context, not the args object.
         args.em,
         args.loss_order,
         args.resolution,
-        args.structure  # path/string to a structure file.
+        args.structure,  # path/string to a structure file.
     )
+
+    # Boltz was trained with this, others might not have been.
+    use_alignment_for_reverse_diffusion = args.model in (BOLTZ_1, BOLTZ_2)
 
     if guidance_type == PURE_GUIDANCE:
         logger.info("Initializing pure guidance")
@@ -348,7 +317,7 @@ def _run_guidance(args: GuidanceConfig | argparse.Namespace, guidance_type: str,
             align_to_input=args.align_to_input,
             partial_diffusion_step=args.partial_diffusion_step,
             out_dir=args.output_dir,
-            alignment_reverse_diffusion=True,  # Boltz was trained with this
+            alignment_reverse_diffusion=use_alignment_for_reverse_diffusion,
             ensemble_size=args.ensemble_size,
         )
 
@@ -366,23 +335,20 @@ def _run_guidance(args: GuidanceConfig | argparse.Namespace, guidance_type: str,
             num_gd_steps=args.num_gd_steps,
             guidance_weight=args.guidance_weight,
             gradient_normalization=args.gradient_normalization,
-            guidance_interval=args.guidance_interval,
+            guidance_interval=args.guidance_interval,  # pyright: ignore (attr added after init intentionally)
             guidance_start=args.guidance_start,
             augmentation=args.augmentation,
             align_to_input=args.align_to_input,
             partial_diffusion_step=args.partial_diffusion_step,
             out_dir=args.output_dir,
-            alignment_reverse_diffusion=True,  # Boltz was trained with this
+            alignment_reverse_diffusion=use_alignment_for_reverse_diffusion,
         )
-
+    else:
+        logger.error(f"Unknown guidance type: {guidance_type}")
+        raise TypeError("Unknown guidance type!")
 
     save_everything(
-        args.output_dir,
-        losses,
-        refined_structure,
-        traj_denoised,
-        traj_next_step,
-        guidance_type
+        args.output_dir, losses, refined_structure, traj_denoised, traj_next_step, guidance_type
     )
 
 
@@ -391,12 +357,12 @@ def epoch_seconds(time_to_convert: datetime) -> float:
 
 
 def get_job_result(
-        args: GuidanceConfig | argparse.Namespace,
-        device: torch.device,
-        started_at: datetime,
-        ended_at: datetime,
-        exit_code: int,
-        status: str
+    args: GuidanceConfig | argparse.Namespace,
+    device: torch.device,
+    started_at: datetime,
+    ended_at: datetime,
+    exit_code: int,
+    status: str,
 ) -> JobResult:
     start_time = epoch_seconds(started_at)
     end_time = epoch_seconds(ended_at)
@@ -406,8 +372,8 @@ def get_job_result(
         method=args.method,
         scaler=args.guidance_type,
         ensemble_size=args.ensemble_size,
-        gradient_weight=args.guidance_weight if hasattr(args, "guidance_weight") else None,
-        gd_steps=args.num_gd_steps if hasattr(args, "num_gd_steps") else None,
+        gradient_weight=args.guidance_weight if hasattr(args, "guidance_weight") else -1.0,
+        gd_steps=args.num_gd_steps if hasattr(args, "num_gd_steps") else -1,
         status=status,
         exit_code=exit_code,
         runtime_seconds=round(end_time - start_time, 2),
@@ -424,18 +390,29 @@ def run_guidance_job_queue(job_queue_path: str) -> list[JobResult]:
         job_queue: list[GuidanceConfig] = pickle.load(fp)
 
     template_job = job_queue[0]
+    if template_job.model_checkpoint is None:
+        raise ValueError("Running guidance requires that you specify a model checkpoint, not None")
+
     logger.info(f"Running {len(job_queue)} jobs, using {template_job} as a setup template")
     device, model_wrapper = get_model_and_device(
-        template_job.device, template_job.model_checkpoint, template_job.model, template_job.method
+        str(template_job.device),
+        template_job.model_checkpoint,
+        template_job.model,
+        template_job.method,
     )
     job_results = []
     for i, job in enumerate(job_queue):
-        logger.info(f"Running job {i+1}/{len(job_queue)}: {job}")
+        logger.info(f"Running job {i + 1}/{len(job_queue)}: {job}")
         # The model wrapper can persist state across runs, so we need to re-initialize it each run.
+        if job.model_checkpoint is None:
+            raise ValueError(
+                "Running guidance requires that you specify a model checkpoint, not None"
+            )
         device, model_wrapper = get_model_and_device(
-            device, job.model_checkpoint, job.model, job.method, model_wrapper.model
+            str(device), job.model_checkpoint, job.model, job.method, model_wrapper.model
         )
 
         job_result = run_guidance(job, job.guidance_type, model_wrapper, device)
+        job_results.append(job_result)
 
     return job_results
