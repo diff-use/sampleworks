2500f717e8f258ab17cc2adef3dd44159c52b0e8
Marcus D. Collins <marcus.collins@astera.org>
2026-07-21 19:40:28 -0700

    removing/modifying tests that are outdated/useless

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -13,7 +13,7 @@ from sampleworks.utils.imports import (
     require_boltz,
     require_protenix,
     require_rf3,
-    RF3_AVAILABLE,
+    RF3_AVAILABLE, PROTPARDELLE_AVAILABLE,
 )
 
 
@@ -32,21 +32,13 @@ class TestAvailabilityFlags:
         """RF3_AVAILABLE should be a boolean."""
         assert isinstance(RF3_AVAILABLE, bool)
 
-    def test_protpardelle_available_is_bool(self):
-        """PROTPARDELLE_AVAILABLE should be a boolean."""
-        assert isinstance(PROTPARDELLE_AVAILABLE, bool)
-
-    def test_at_least_one_available(self):
-        """At least one model should be available in test environment."""
-        assert BOLTZ_AVAILABLE or PROTENIX_AVAILABLE or RF3_AVAILABLE or PROTPARDELLE_AVAILABLE
-
 
 class TestCheckFunctions:
     """Test the check_*_available helper functions."""
 
     def test_check_any_model_available_no_error(self):
         """check_any_model_available should not raise if any model available."""
-        if BOLTZ_AVAILABLE or PROTENIX_AVAILABLE or RF3_AVAILABLE:
+        if BOLTZ_AVAILABLE or PROTENIX_AVAILABLE or RF3_AVAILABLE or PROTPARDELLE_AVAILABLE:
             check_any_model_available()
 
     def test_check_boltz_available(self):
